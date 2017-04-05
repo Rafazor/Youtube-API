3dfa891e6ba31f29be897bea3f5db294faef0638
Rafa Calin <rafa.calin@yahoo.com>
2017-04-06 01:10:31 +0300

    add youtube key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+const API_KEY = 'AIzaSyCvuJyjsiiK1mnvTgO6QrmRyAW681AdMec';
 
 const App = () => {
-    return <div>Salut!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
-ReactDOM.render(<App/>, document.querySelector(".container"));
+ReactDOM.render(<App />, document.querySelector(".container"));
