f8e680b147e83ed2e60c29f5dcd41ee3c64fc961
Rafa Calin <rafa.calin@yahoo.com>
2017-04-10 16:52:44 +0300

    add youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 const API_KEY = 'AIzaSyCvuJyjsiiK1mnvTgO6QrmRyAW681AdMec';
+
+YTSearch({key: API_KEY, term: 'Zalau'}, function(data) {
+    console.log(data);
+})
 
 const App = () => {
     return (
