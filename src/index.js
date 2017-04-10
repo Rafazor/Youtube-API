5186a2810a90564f7b1f576f2f42e50cc57e5e97
Rafa Calin <rafa.calin@yahoo.com>
2017-04-10 16:53:32 +0300

    youtube api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'
 const API_KEY = 'AIzaSyCvuJyjsiiK1mnvTgO6QrmRyAW681AdMec';
 
-YTSearch({key: API_KEY, term: 'Zalau'}, function(data) {
-    console.log(data);
-})
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+        this.state = { videos: [] };
+
+        YTSearch({ key: API_KEY, term: 'Zalau' }, function (videos) {
+            this.setState({ videos });
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    };
+
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
